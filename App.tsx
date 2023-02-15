ec86662b8a7c157d60b851a5ed6ea32855f7ef48
howdyfrom2019 <tjd1emd@naver.com>
2023-02-15 14:59:00 +0900

    bugFix: figure dispatch in useSSE.ts

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, { useEffect, useRef } from "react";
+import React, {useCallback, useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -17,15 +17,46 @@ import {
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import HorizontalScrollView from './src/Components/HorizontalScrollView';
-import { useSSE } from "./src/Hooks/useSSE";
+import {auctionType, SSEProps, useSSE} from './src/Hooks/useSSE';
 
 function App(): JSX.Element {
-  const stream = useSSE();
+  const [SSETarget, setSSETarget] = useState<auctionType | null>(null);
+
+  const itemClickedListener = useCallback((e: Event) => {
+    const {data} = e as unknown as SSEProps;
+    const auctionItem: auctionType = JSON.parse(data);
+    setSSETarget(auctionItem);
+  }, []);
+
+  const [artwork, setArtWork] = useSSE(itemClickedListener);
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  const onChangeArtwork = useCallback(
+    (target: auctionType) => {
+      const {auctionId} = target;
+      setArtWork(prev => {
+        return prev.map(v => {
+          if (v.auctionId === auctionId) {
+            return target;
+          }
+          return v;
+        });
+      });
+    },
+    [setArtWork],
+  );
+
+  useEffect(() => {
+    if (SSETarget) {
+      onChangeArtwork(SSETarget);
+    }
+  }, [SSETarget, onChangeArtwork]);
+
+  console.log(artwork);
 
   return (
     <SafeAreaView style={[backgroundStyle, styles.container]}>
--- src/Hooks/useSSE.ts
+++ src/Hooks/useSSE.ts
@@ -1,14 +1,40 @@
 import { RNEventSource } from 'rn-eventsource-reborn';
-import { useCallback, useEffect } from 'react';
+import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
 
-export function useSSE(): any {
+export interface SSEProps {
+  data: string;
+  isTrusted: boolean;
+  lastEventId: string;
+}
+
+export interface auctionType {
+  auctionId: number;
+  viewCount: number;
+}
+
+type ArtWorksType = auctionType[];
+
+export function useSSE(
+  callback: (e: Event) => void,
+): [ArtWorksType, Dispatch<SetStateAction<ArtWorksType>>] {
+  const [currWeekArts, setCurrWeekArts] = useState<ArtWorksType>([]);
+
+  const initArtData = useCallback(() => {
+    let start = 2123;
+    const targets: ArtWorksType = Array.from({ length: 20 }, (_, i) => ({ auctionId: start + i, viewCount: -1 }));
+    setCurrWeekArts(targets);
+  }, []);
+
+  useEffect(() => {
+    initArtData();
+  }, [initArtData]);
 
   useEffect(() => {
     const source = new RNEventSource(
       'https://api.fleaauction.world/v2/sse/event',
     );
-    source.addEventListener('sse.auction_viewed', (e) => console.log(e));
-  }, []);
+    source.addEventListener('sse.auction_viewed', callback);
+  }, [callback]);
 
-  return 1;
+  return [currWeekArts, setCurrWeekArts];
 }
